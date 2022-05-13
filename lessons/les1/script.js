1b24f60504b3a9a7347004762f9ccdc5cecaa53b
Anton Katonov <antonkatonov@gmail.com>
2022-05-13 10:09:55 +0300

    data base add ganre

--- lessons/les1/script.js
+++ lessons/les1/script.js
@@ -1,1 +1,74 @@
-console.log(1);
+'use strict';
+
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('на сколько оцениваете его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+
+function detectPersonalMovie() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классный зритель");
+  }  else if (personalMovieDB.count >= 30) {
+    console.log("Вы Киноман");
+  } else  {
+    console.log("Произошла ошибка");
+  }
+}
+
+detectPersonalMovie();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGeners() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGeners();
+
+console.log(personalMovieDB);
